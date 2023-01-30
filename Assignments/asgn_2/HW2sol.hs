0d3f2cbdaa61c0870f81d5b1a52cd46c95e8f3bf
Ya Zou <zouy2@os1.engr.oregonstate.edu>
2023-01-29 18:59:00 -0800

    files modified

--- Assignments/asgn_2/HW2sol.hs
+++ Assignments/asgn_2/HW2sol.hs
@@ -6,7 +6,8 @@ import HW2types
 -- Email:zouy2@oregonstate.edu    * 
 -- Date: Jan,22,2023              *
 -- ********************************
- 
+
+
 
 -- ==============================================================
 -- ===============Exercise 1. Programming with Lists=============
@@ -21,24 +22,28 @@ ins x ((y,n):ys)
   | otherwise   = (y,n):ins x ys
 
 
-
 -- (b) Define the function del that removes an element from a multiset.
 
 del :: Eq a => a -> Bag a -> Bag a
-del el [] = []
-del el ((b1, 1):bs) | el == b1 = bs
-                    | otherwise = (b1,1):(del el bs)
-del el (b:bs) | el == fst b = (el, snd b - 1):bs
-              | otherwise = b:(del el bs)
+del _ [] = []
+del el ((b1, 1):bs)
+  | el == b1 = bs
+  | otherwise = (b1, 1) : del el bs
+del el (b:bs)
+  | el == fst b = (fst b, snd b - 1) : bs
+  | otherwise = b : del el bs
+
 
 
 -- (c)Define a function bag that takes a list of values and produces a multiset representation.
 
+
+bag :: Eq a => [a] -> Bag a
 emptyBag :: Bag a
 emptyBag = []
-bag :: Eq a => [a] -> Bag a
 bag elts = foldr add emptyBag elts
 
+-- helper function
 add :: Eq a => a -> Bag a -> Bag a
 add x [] = [(x, 1)]
 add x ((y,n):ys) 
@@ -46,7 +51,8 @@ add x ((y,n):ys)
   | otherwise = (y,n):add x ys
 
 -- (d) Define a function subbag that determines whether or not its first argument bag is contained in the second.
--- *****
+
+
 subbag :: (Eq a )=> Bag a -> Bag a -> Bool
 subbag [] _ = True
 subbag ((x,n):xs) ys = case lookup x ys of
@@ -57,35 +63,46 @@ subbag ((x,n):xs) ys = case lookup x ys of
 --(e) Define a function isSet that tests whether a bag is actually a set,which is the case when each element occurs only once.  isSet :: Eq a => Bag a -> Bool
 
 isSet :: Eq a => Bag a -> Bool
-isSet bag = all (\x -> count x bag == 1) bag
-  where count x = length . filter (== x)
+isSet [] = True
+isSet ((_, count):xs)
+  | count /= 1 = False
+  | otherwise = isSet xs
 
 
--- ==============================================================
--- ====================Exercise 2. Graphs========================
+-- (f) Define a function size that computes the number of elements contained in a bag. size :: Bag a -> Int
+
+size :: Bag a -> Int
+size [] = 0
+size xs = sum [y|(x,y) <- xs]
 
 
---(a) Define the function nodes :: Graph -> [Node] that computes the list of nodes contained in a given graph. For example, nodes g = [1,2,3,4].
+--  ====================Exercise 2. Graphs========================
+
+-- let g = [ (1,2), (1,3), (2,3), (2,4),(3,4)]
+-- let h = [ (1,2), (1,3), (2,1), (3,2), (4,4) ]
+-- let g2 = [ (1,5), (1,7), (2,5), (7,2) ]
+
+-- (a) Define the function nodes :: Graph -> [Node] that computes the list of nodes contained in a given graph. For example, nodes g = [1,2,3,4].
 
 nodes :: Graph -> [Node]
 nodes g = norm $ concat [[fst a, snd a] | a <- g]
 
 --(b) Define the function suc :: Node -> Graph -> [Node] that computes the list of successors for a node in a given graph. For example,suc 2 g = [3,4],suc 4 g = [], andsuc 4 h = [4].
 
--- suc :: Node -> Graph -> [Node]
 
+suc :: Node -> Graph -> [Node]
+suc h g = [y | (x,y) <- g, x == h]
 
-
+   
 -- (c) Define the function detach :: Node->Graph->Graph that removes a node together with all of its incident edges from a graph.For example, detach 3 g = [(1,2),(2,4)] and detach 2 h = [(1,3),(4,4)].
 
--- ********************//
+
 detach :: Node -> Graph -> Graph
-detach n g = [(x,y) | (x,y) <- g, not (x == n || y == n)]
+detach h g = [(x,y) | (x,y) <- g, not (x == h || y == h)]
 
 
+-- (d)Define the function cyc :: Int -> Graph that creates a cycle of any given number. For example, cyc 4 = [(1,2),(2,3),(3,4),(4,1)].
 
---(d)Define the function cyc :: Int -> Graph that creates a cycle of any given number. For example, cyc 4 = [(1,2),(2,3),(3,4),(4,1)].
--- ****************tested
 cyc :: Int -> Graph
 cyc n = zip [1..n] (drop 1 (cycle [1..n]))
 
@@ -94,10 +111,11 @@ cyc n = zip [1..n] (drop 1 (cycle [1..n]))
 -- ===============Exercise 3. Programming with Data Type=========
 
 
+
 -- (a)Define the function width that computes the width of a shape.
 
 width :: Shape -> Length
-width (Pt _) = 0
+width (Pt _ ) = 0
 width (Circle _ r) = 2*r
 width (Rect _ w h) = w
 
@@ -124,6 +142,7 @@ minX (Rect (x,_) _ _) = x
 -- (d) Define a function move that moves the position of a shape by a vector given by apoint a sits second argument.
 --    move :: Shape -> Point -> Shape
 -- It is probably a good idea to define and use an auxiliary function addPt :: Point -> Point -> Point, which adds two points component wise.
+ 
 
 addPt :: Point -> Point -> Point
 addPt (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
@@ -132,9 +151,3 @@ move :: Shape -> Point -> Shape
 move (Pt p) d       = Pt (addPt p d)
 move (Circle p r) d = Circle (addPt p d) r
 move (Rect p w h) d = Rect (addPt p d) w h
-
-
-
-
-
-
--- Assignments/asgn_2/HW2types.hs
+++ Assignments/asgn_2/HW2types.hs
@@ -2,7 +2,14 @@ module HW2types where
 
 import Data.List 
 
-
+b4 :: Bag Int
+b4 = []
+b1 :: Bag Int
+b1 = [(3,2), (4,1),(7,3),(12,5)]
+b2 :: Bag Int
+b2 = [(3,1), (7,2)]
+b3 :: Bag Int
+b3 = [(3,1), (4,1),(7,1),(10,1) ]
 -- Types for Exercise 1
 --
 type Bag a = [(a,Int)]
