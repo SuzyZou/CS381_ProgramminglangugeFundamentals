3cbb44fe478e9fb4d04e95b9c0e79a83796558ed
Suzy_Zou <suzyzou@10-251-47-212.wireless.oregonstate.edu>
2023-02-13 12:02:30 -0800

    Hw4 completed

--- Assignments/asgn_4/stacklang2.hs
+++ Assignments/asgn_4/stacklang2.hs
@@ -1,7 +1,7 @@
--- Student Name : Ya Zou
-
+module Stacklang2 where
 --   ************Part 2: Stack Language 2***
 --   ************Operations****************
+
 
 -- LDI Int – loads its integer parameter onto the stack (replaces LD)
 -- LDB Bool – loads its boolean parameter onto the stack.
@@ -54,6 +54,7 @@ sem MULT _ =Nothing
 sem DUP [] = Nothing
 sem DUP  (x:s)  = Just (x:(x:s))
 
+
 sem (IFELSE p1 p2) (x:s) = case x of
                             Left True  -> run p1 s
                             Left False -> run p2 s
@@ -65,17 +66,3 @@ run (x:xs) s = case sem x s of
              Nothing -> Nothing
              Just r -> run xs r
 
-
--- ***********************Teset info*******************
--- stack1 :: Stack
--- stack1 = [Right 1, Right 3, Right 5, Right 7, Right 9]
--- stack2 :: Stack
--- stack2 = [Left True,Right 3]
--- test1 = [LDI 3,DUP,ADD,DUP,MULT]
--- test2 = [LDB True, DUP, IFELSE[LDI 1][LDI 0]]
--- test3 = [LEQ]
--- test4 = [ADD,ADD,MULT,DUP]
--- test5 = [LEQ,IFELSE [] [], LDI 9]
--- test6 = [LDI 5, LDI 2, LEQ, IFELSE [LDI 10, DUP] [], ADD]
--- test7 = [LDI 5, LDI 7, LEQ, IFELSE [LDI 10, DUP] [LDI 20, DUP],ADD]
--- test8 = [LDI 1, LDI 2, LDI 3, LDI 4, LDI 5, ADD, ADD, ADD, ADD]
