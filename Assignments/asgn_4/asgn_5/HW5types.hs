cf47116a68652cfcc64e2f67298bd5feb1098c49
Ya Zou <114107089+SuzyZou@users.noreply.github.com>
2023-02-27 19:41:13 -0800

    group assignment is completed
    This assignment is a group assignment that I worked with other 2 students

---
+++ Assignments/asgn_4/asgn_5/HW5types.hs
@@ -0,0 +1,35 @@
+-- Homework 5 (types)
+module HW5types where
+import Data.Maybe
+
+type Prog = [Cmd]
+
+data Cmd = LDI Int 
+         | ADD 
+         | MULT 
+         | DUP 
+         | DEC 
+         | SWAP 
+         | POP Int 
+         | IFELSE Prog Prog
+         | LDB Bool 
+         | LEQ
+         deriving Show
+
+data Val = I Int | B Bool 
+   deriving Show
+
+
+type Stack = [Val]
+
+data Result = A Stack | RankError | TypeError
+           deriving Show
+
+type D = Stack -> Stack
+type Rank = Int
+type CmdRank = (Int, Int)
+
+
+-- Add -> stack rank >= 2 rank # of elemetns on stack
+-- run [ADD] [I5] ->stack error
+-- 
